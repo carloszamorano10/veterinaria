ff240aee7f3f42dfb946c904d0b6a89ea2c43811
carloszamorano10 <ca.zamorano@hotmail.com>
2025-06-28 13:48:45 -0400

    fin

--- index.js
+++ index.js
@@ -1,22 +1,16 @@
-
 const { registrar, leer } = require("./operaciones");
-
-
 
 const argumentos = process.argv.slice(2);
 
 const operacion = argumentos[0];
 
 if (operacion === "registrar") {
-   
-  
-
   const datosCita = {
     nombre: argumentos[1],
-    edad: parseInt(argumentos[2]), 
+    edad: parseInt(argumentos[2]),
     tipo: argumentos[3],
     color: argumentos[4],
-    enfermedad: argumentos[5]
+    enfermedad: argumentos[5],
   };
 
   registrar(datosCita);
